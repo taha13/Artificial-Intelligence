4ffddfd3a14f40bff5497f4d4fef7ac8204a90ca
Taha Siddiqui <taha.siddiqui01@gmail.com>
2017-11-30 01:33:44 +0500

    First Assignment

---
+++ assignment2.py
@@ -0,0 +1,113 @@
+# D. verbing
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+import math
+import re
+def verbing(s):
+    if(len(s) < 3):
+        return s
+    else:
+        if (s[-3:] == 'ing'):
+            s += 'ly'
+        else:
+            s += 'ing'
+        return s
+
+
+# E. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+def not_bad(s):
+    split = s.split()
+    i = split.index('not')
+    # done = 0
+    se = re.findall(r'bad[!]*', s)
+    if se:
+        se = se[0]
+    else:
+        return s
+    for bading in split:
+        if (bading == se):
+            j =  split.index(se)
+            if(i < j):
+                del split[i:j+1]
+                if re.search(r"!" , se):
+                    return ' '.join(split) + ' good!'
+                else:
+                    return ' '.join(split) + ' good'
+            else:
+                return s
+
+
+
+    # j = s.index(se[0])
+    # del split[i:j+1]
+    # return ' '.join(split) + ' good!'
+
+
+# F. front_back
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+def front_back(a, b):
+    no = len(a)
+    diveno =math.ceil(no/2)
+
+    no2 = len(b)
+    diveno2 = math.ceil(no2 / 2)
+
+    firstparta, secondparta = a[:diveno], a[diveno:]
+    # print(firstparta + ' ' + secondparta)
+    firstpartb, secondpartb = b[:diveno2], b[diveno2:]
+    # print(firstpartb + ' ' + secondpartb)
+
+    return firstparta+firstpartb+secondparta+secondpartb
+
+
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# main() calls the above functions with interesting inputs,
+# using the above test() to check if the result is correct or not.
+def main():
+  print ('verbing')
+  test(verbing('hail'), 'hailing')
+  test(verbing('swiming'), 'swimingly')
+  test(verbing('do'), 'do')
+
+  print('\n')
+  print ('not_bad')
+  test(not_bad('This movie is not so bad'), 'This movie is good')
+  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+  test(not_bad('This tea is not hot'), 'This tea is not hot')
+  test(not_bad("It's bad yet not"), "It's bad yet not")
+
+  print('\n')
+  print ('front_back')
+  test(front_back('abcd', 'xy'), 'abxcdy')
+  test(front_back('abcde', 'xyz'), 'abcxydez')
+  test(front_back('Kitten', 'Donut'), 'KitDontenut')
+
+if __name__ == '__main__':
+  main()
